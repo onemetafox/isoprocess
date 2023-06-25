74ed438c36dd5db1c729251c90d96b196a3b6ccd
todaycodemaster <seniordev1994128@gmail.com>
2023-06-25 10:59:50 -0700

    remove datatable setting in edit check list

--- application/views/employee/edit_checklist.php
+++ application/views/employee/edit_checklist.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= $title ?></title>
-<!--    <link href="http://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">-->
     <link href="<?= base_url(); ?>assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
     <link href="<?= base_url(); ?>assets/css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="<?= base_url(); ?>assets/css/core.css" rel="stylesheet" type="text/css">
@@ -24,34 +23,8 @@
     <script type="text/javascript" src="<?= base_url(); ?>assets/js/plugins/tables/datatables/datatables.min.js"></script>
     <script type="text/javascript" src="<?= base_url(); ?>assets/js/core/app.js"></script>
     <script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootbox.min.js"></script>
-<!--    <script type="text/javascript" src="--><?//= base_url(); ?><!--assets/js/pages/datatables_basic.js"></script>-->
 
-    <script type="text/javascript">
-        $(function() {
-            // Setting datatable defaults
-            $.extend( $.fn.dataTable.defaults, {
-                autoWidth: false,
-                columnDefs: [{
-                    orderable: false,
-                    width: '150px',
-                    targets: [6 ]
-                }],
-                dom: '<"datatable-header"fl><"datatable-scroll"t><"datatable-footer"ip>',
-                language: {
-                    search: '<span>Filter:</span> _INPUT_',
-                    searchPlaceholder: 'Type to filter...',
-                    lengthMenu: '<span>Show:</span> _MENU_',
-                    paginate: { 'first': 'First', 'last': 'Last', 'next': '&rarr;', 'previous': '&larr;' }
-                },
-                drawCallback: function () {
-                    $(this).find('tbody tr').slice(-3).find('.dropdown, .btn-group').addClass('dropup');
-                },
-                preDrawCallback: function() {
-                    $(this).find('tbody tr').slice(-3).find('.dropdown, .btn-group').removeClass('dropup');
-                }
-            });
-        });
-    </script>
+   
 </head>
 
 <body class="navbar-top">
@@ -69,7 +42,6 @@
         <!-- Main sidebar -->
         <?php $this->load->view('employee/sidebar'); ?>
         <!-- /main sidebar -->
-
 
         <!-- Main content -->
         <div class="content-wrapper">
@@ -94,21 +66,6 @@
                             ?>
                             <img src="<?php echo base_url(); ?>uploads/logo/<?= $audito ?>" style="height:50px;">
                             <span class="text-semibold"><?= $title ?></span>
-
-                            <div class="pull-right">
-                                <?php
-                                /*                                $consultant_id = $this->session->userdata('consultant_id');
-                                                                $plan_ids1 = @$this->db->query("select * from upgrad_plan where `consultant_id`='$consultant_id' AND `status`='1'")->row()->plan_id;
-                                                                if (count($plan_ids1) > 0) {
-                                                                    $d1 = @$this->db->query("select * from plan where `plan_id`='$plan_ids1'")->row()->no_of_user;
-                                                                }
-                                                                $d2 = @$this->db->query("select * from plan order by no_of_user DESC")->row()->plan_id;
-                                                                */?><!--
-                                <?php /*if ($d1 != $d2 && $d2 > $d1) { */?>
-                                    <a href="<?php /*echo base_url(); */?>index.php/Auth/update_process"
-                                       class="btn bg-brown"> <i class="icon-wrench" title="Main pages"></i> <span> Upgrade Plan</span></a>
-                                --><?php /*} */?>
-                            </div>
                         </h4>
                     </div>
                 </div>
@@ -133,7 +90,6 @@
 
             <!-- Content area -->
             <div class="content">
-                <!-- Basic datatable -->
                 <?php
                 foreach ($checklist as $checklist) { ?>
                 <div class="panel panel-body text-left" id="main-data">
