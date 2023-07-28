ed1dac404c80e24465be1daa22660b158b472cb7
todaycodemaster <seniordev1994128@gmail.com>
2023-07-28 11:03:23 -0700

    fix: manage modal not working error fixed

--- application/views/employee/edit_checklist.php
+++ application/views/employee/edit_checklist.php
@@ -326,6 +326,12 @@
 
 <script type="text/javascript">
     var process_id = 0;
+    if( Date.now() <= Date.parse('<?=$start_time?>')){
+        $('#e_a1').prop("disabled", true);
+        $('#e_a2').prop("disabled", true);
+        $('#e_a3').prop("disabled", true);
+        $('#e_a4').prop("disabled", true);
+    }
     $('#a_t1').prop("disabled", true);
     $('#a_t2').prop("disabled", true);
     $('#a_t3').prop("disabled", true);
